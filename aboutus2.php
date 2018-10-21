f54a5bc9b6d90178e2a09095246ae0a2b14610d2
olaseth39 <sethabiola@yahoo.com>
2018-10-21 21:32:10 +0100

    aboutus2.php

---
+++ aboutus2.php
@@ -0,0 +1,90 @@
+<section>
+      <div class="container pb-30">
+        <div class="section-content">
+          <div class="row">
+            <div class="col-md-12">
+            
+              <div class="meet-doctors">
+                  <h2 class="mt-0 text-uppercase font-28 line-bottom line-height-1">Our <span class="text-theme-color-2 font-weight-400 w3-text-green">Uniqueness</span></h2>
+
+              </div>
+              <div class="row mb-sm-30">
+                <div class="col-sm-6 col-md-6">
+                  <div class="icon-box p-0 mb-20">
+                   <a class="icon bg-theme-colored icon-circled icon-border-effect effect-circle icon-sm pull-left sm-pull-none flip" href="#">
+                    <i class="pe-7s-link text-white"></i>
+                   </a>
+                   <div class="ml-70 ml-sm-0">
+                    <h5 class="icon-box-title mt-10 text-uppercase letter-space-2 mb-10">Strong Industrial Link</h5>
+                    <p class="text-gray">We offer job assistance for our graduands</p>
+                   </div>
+                  </div>
+                </div>
+                <div class="col-sm-6 col-md-6">
+                  <div class="icon-box p-0 mb-20">
+                   <a class="icon bg-theme-color-2 icon-circled icon-border-effect effect-circle icon-sm pull-left sm-pull-none flip" href="#">
+                    <i class="pe-7s-pen text-white"></i>
+                   </a>
+                   <div class="ml-70 ml-sm-0">
+                    <h5 class="icon-box-title mt-10 text-uppercase letter-space-2 mb-10">Experienced and Vast Software Developers as Trainers</h5>
+<p class="text-gray">Our trainers are professionals who have practiced for years in the industry and have developed enterprise solutions</p>
+                   </div>
+                  </div>
+                </div>
+                <div class="col-sm-6 col-md-6">
+                  <div class="icon-box p-0 mb-20">
+                   <a class="icon bg-theme-color-2 icon-circled icon-border-effect effect-circle icon-sm pull-left sm-pull-none flip" href="#">
+                    <i class="pe-7s-tools text-white"></i>
+                   </a>
+                   <div class="ml-70 ml-sm-0">
+                    <h5 class="icon-box-title mt-10 text-uppercase letter-space-2 mb-10">Effective Curriculum</h5>
+                    <p class="text-gray">Our curriculum  produces high quality software developers within the shortest possible time  even with little or no prior knowledge in software development</p>
+                   </div>
+                  </div>
+                </div>
+                <div class="col-sm-6 col-md-6">
+                  <div class="icon-box p-0 mb-20">
+                   <a class="icon bg-theme-colored icon-circled icon-border-effect effect-circle icon-sm pull-left sm-pull-none flip" href="#">
+                    <i class="pe-7s-light text-white"></i>
+                   </a>
+                   <div class="ml-70 ml-sm-0">
+                    <h5 class="icon-box-title mt-10 text-uppercase letter-space-2 mb-10">Mentorship</h5>
+                    <p class="text-gray">Our trainers will also serve as mentors and guide to the participants even after their programme with us to accelerate their career path and make their journey seamless in the industry.</p>
+                   </div>
+                  </div>
+                </div>
+   <div class="col-sm-6 col-md-6">
+                  <div class="icon-box p-0">
+                   <a class="icon bg-theme-colored icon-circled icon-border-effect effect-circle icon-sm pull-left sm-pull-none flip" href="#">
+                    <i class="fa fa-thumbs-o-up text-white"></i>
+                   </a>
+                   <div class="ml-70 ml-sm-0">
+                    <h5 class="icon-box-title mt-10 text-uppercase letter-space-2 mb-10">Friendly and Conducive Environment</h5>
+                    <p class="text-gray">Our training rooms are fully equipped with modern facilities together with the friendly ambience created by our amicable trainers and other members of staff.</p>
+                   </div>
+                  </div>
+                </div>
+                <div class="col-sm-6 col-md-6">
+                  <div class="icon-box p-0">
+                   <a class="icon bg-theme-color-2 icon-circled icon-border-effect effect-circle icon-sm pull-left sm-pull-none flip" href="#">
+                    <i class="fa fa-suitcase text-white"></i>
+                   </a>
+                   <div class="ml-70 ml-sm-0">
+                    <h5 class="icon-box-title mt-10 text-uppercase letter-space-2 mb-10">Project-based Teaching</h5>
+                    <p class="text-gray">Participants deliver miniprojects at the end of each module. This also gives them the opportunity to build project portfolio while still at the boot camp</p>
+                   </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</section>
+
+
+
+<input type="email" name="text" class="w3-bar-item w3-input w3-white" placeholder="Your email..." size="30px" value="">
+      <button type="submit" class="w3-button w3-bar-item w3-black">
+      Subscribe</button>
+      </div>
