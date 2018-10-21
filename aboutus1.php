505d8a8962f25d8f76cdf39a3ae43f4c94e84d61
olaseth39 <sethabiola@yahoo.com>
2018-10-21 21:29:17 +0100

    aboutus1.php

---
+++ aboutus1.php
@@ -0,0 +1,20 @@
+ <!-- section about -->
+ <section id="about">
+	<div clas="w3-container" style="padding-bottom: 2px;">
+		<div class="section-content">
+			<div class="w3-row-padding">
+				<div class="col-md-12">
+					<h6 class="letter-space-3 mt-20 mb-0">ABOUT ADELOKHE AGRIC CONCEPT</h6>
+					<h1 class="text-uppercase font-weight-600 mt-0 font-28 line-bottom">We are producing healthy and tasty food</h1>
+					<p>Adelokhe Agric Concept was birthed to solve two major problems in the agricultural industry:
+					<ul class="list theme-colored check">
+					<li> To bridge the gap between the academics and the industry by making as many unemployed to become employable applicants</li>
+					<li>To improve the quality of locally made food by raising good crops that will inculcate globally acceptable best practices in thei taste</li></ul>
+					<a class="btn btn-success btn-flat pull-right btn-lg mt-10 mb-sm-30" href="">Know More →</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- section features -->
