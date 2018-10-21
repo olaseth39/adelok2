9041f3968f2f7a9bfe72bcb1cfe534f40f864241
olaseth39 <sethabiola@yahoo.com>
2018-10-21 21:27:20 +0100

    footer.php

---
+++ footer.php
@@ -0,0 +1,54 @@
+
+<div class="w3-bar">
+	<div class="w3-card-4 w3-border w3-topbar w3-border-black w3-black">
+		<div class="w3-row-padding" style="padding:20px">
+			<div class="w3-third">
+				<h6>Contact Us</h6>
+				<p>Adelokhe Agric Concept<br>Company Address<br>
+				Ogun State, Niger</p>
+				<span>+company number1,<br>+companynumber2<br>+company number3,<br>
+				emailaddress</span>
+				<p>Company intro</p>
+			</div>
+			<div class="w3-third">
+				<h6 class="w3-text-white">QUICK LINK</h6>
+				<div class="w3-text-white">
+					<p>FBS video tour</p>
+					<p>Apply Online</p>
+					<p>Curriculum</p>
+				</div>
+				<p>FBS social link</p>
+					<img src="images/twitter_logo.PNG" alt="fb" style="width: 20px; height: 20px">
+					<img src="images/whatsapp_logo.PNG" alt="fb" style="width: 20px; height: 20px">
+					<img src="images/fb.PNG" alt="fb" style="width: 20px; height: 20px">
+			</div>
+			<div class="w3-third">
+				<h6>RECENT NEWS</h6>
+				<div class="w3-white">
+					<a href="" style="text-decoration: none"><p>text text text text text text tex tex t ext text text text text text tex tex t ext text text text text text tex tex t ext text text text text text tex tex t</p></a>
+				</div>
+			</div>
+		</div>
+		<div class="w3-border w3-topbar w3-border-white w3-display-container">
+			<br>
+			<p>Copyright &copy;<?php  date_default_timezone_set('africa/lagos');echo date('Y'); ?> Adelokhe Agric Concept. All Rights Reserved
+			
+				<span class="w3-display w3-right">
+					Designed by Abseth Technology
+				</span>
+			</p>
+			
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript" src="bootstrap/js/jquery-3.1.1.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+<script type="text/javascript">
+	var className = document.getElementsByClassName('image');
+	className.onmouseover = showeffect;
+	function showeffect(){
+		className.style.height = "500";
+
+	}
+</script>
