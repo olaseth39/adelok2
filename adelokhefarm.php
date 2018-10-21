376035d9a5c658b852cb5d18d7a9f9efc1f0ca8c
olaseth39 <sethabiola@yahoo.com>
2018-10-21 21:35:45 +0100

    adelokhefarm.php

---
+++ adelokhefarm.php
@@ -0,0 +1,30 @@
+<?php include('header3.php'); ?>
+
+<div class="row w3-container">
+			<div class="col-md-12 w3-display-container w3-text-white">
+				<img src="images/cateringservice2.png" id="cateringimage2">
+				<div class="w3-display-middle w3-large">
+					<h2>Since 2000</h2>
+				</div>
+			</div>			
+</div>
+<br>
+<div class="row">
+	<div class="col-md-6">
+		<p id="largefont">
+			The option of modern outdoor catering is offered by SweetHour Pot Limited which takes the tedious task of party planning out of your hands and in its place, the full Sweet treatment is provided.
+			<br>
+			So for your next event look no further than SweetHour Pot to make your wedding receptions, birthday parties, graduation ceremonies, inaugural parties, housewarmings, naming/dedication ceremonies, AGMs, end of the year parties, industrials and other functions come alive.
+			<br>
+			Our prices are competitive for the vast amenities we provide.
+			<br><br><br>
+			<b>Location</b>: 2nd Avenue, 22 Road, Festac Town.
+			<br>
+			<b>For enquiries</b>: 09057754868, 09073404074.
+			<br>
+			E-mail: <a href="SweetHourpot@gmail.com.ng">SweetHourpot@gmail.com.ng</a>
+		</p>	
+	</div>	
+</div>
+<hr>
+<?php include('footer.php'); ?> 
