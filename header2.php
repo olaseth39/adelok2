2b7005a23339d8fa8529d353a02bfab587917324
olaseth39 <sethabiola@yahoo.com>
2018-10-21 21:24:28 +0100

    header.php

---
+++ header2.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Adelokfamz</title>
+	<title>Header</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width" initial-scale=1">
+	<meta name="description" content="Moat Academy - Accelerating your career in the software industry through our coding bootcamp" />
+	<meta name="keywords" content="restaurant, farm, agricultural practices, farm processing factory, animal produce, fruits, food, animal, food ordering,delta,nigeria,adelokhe agric concept, processing factory, market, rice, beans, order online, Abiola Daropale, web design" />
+	<meta name="author" content="Adelokhe Agricultural Concept" />
+	<!-- Bootstrap.css -->
+	<link rel="stylesheet" type="text/css" href="Bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="adelokfamz.css">
+	<link rel="stylesheet" type="text/css" href="w3.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="w3-display-container w3-col m6">
+		
+		<img src="images/farmlogo.jpg" alt="school image" class="w3-image w3-round w3-opacity w3-hover-opacity-off" style="width: 100%; height: 300px">
+		<div class="w3-display-middle w3-container w3-text-black" style="font-size: 40px">ADELOKHE AGRIC CONCEPT<br><b>
+			<p style="font-size: 18px; font-weight: 50; background-color: rgba(0,255,0,0.6);" class="w3-bar w3-leftbar w3-border-black w3-text-white">...Providing healthy and tasty foods
+			</p></b>
+		</div>
+		<div class="w3-display-topright w3-container"><img src="images/twitter_logo.PNG" alt="fb" style="width: 20px; height: 30px">
+		<img src="images/whatsapp_logo.PNG" alt="fb" style="width: 20px; height: 30px">
+		<img src="images/fb.PNG" alt="fb" style="width: 20px;height: 30px">
+		</div>
+		<div class="w3-display-left w3-container w3-margin-left"><img src="images/schoollogo.png" alt="schoollogo" style="width: 200px">
+		</div>
+		
+	</div>
+	<div class="w3-col m6">
+		<div class="w3-content w3-section" style="max-width:100%">
+			<div class="w3-display-container mySlides">
+				  <img class="w3-animate-top" src="images/snacks.png" style="width:100%">
+				  <div class="w3-display-bottomleft w3-container w3-padding-16 w3-black">
+				  		Adelokhe Farm
+				  </div>
+			 </div>
+			 <div class="w3-display-container mySlides">
+				  <img class="w3-animate-bottom" src="images/cake.jpg" style="width:100%">
+				  <div class="w3-display-bottomleft w3-container w3-padding-16 w3-black">
+					  		Adelokhe restaurant
+					</div>
+			</div>
+			 <div class="w3-display-container mySlides">
+				  <img class="w3-animate-top" src="images/whiterice.png" style="width:100%">
+				  <div class="w3-display-bottomleft w3-container w3-padding-16 w3-black">
+						  		Adelokhe Farm
+					</div>
+			</div>
+			 <div class="w3-display-container mySlides">
+				  <img class="w3-animate-bottom" src="images/smallchops.png" style="width:100%">
+				   <div class="w3-display-bottomleft w3-container w3-padding-16 w3-black">
+							  		Adelokhe Farm
+					</div>
+			</div>
+		</div>
+	</div>
+<script type="text/javascript" src="bootstrap/js/jquery-3.1.1.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+<script>
+var myIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2500);    
+}
+</script>
+
+	</div>
+	<!-- column for navigation -->
+	<div class="top">
+	<div class="w3-bar w3-topbar w3-border-black w3-padding-16" style="background-color: rgba(0,255,0,0.8);">
+		<div class="w3-dropdown-hover w3-black">
+			<a href="index.php"><button class="w3-button w3-font-small w3-text-white" style="padding-left: 30px">HOME&nbsp;&raquo;</button></a>
+			<!-- <div class="w3-dropdown-content w3-bar-block w3-card-4 w3-teal w3-topbar w3-border-black">
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Vision & Mission</a>
+				
+			</div> -->
+		</div>
+		<div class="w3-dropdown-hover">
+			<button class="w3-button w3-text-white">ABOUT US&nbsp;&raquo;</button>
+			<!-- <div class="w3-dropdown-content w3-bar-block w3-card-4 w3-teal w3-topbar w3-border-black">
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Curriculum</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Working at FBS</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Learning Resources</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Centre</a>
+				
+			</div> -->
+		</div>
+		<div class="w3-dropdown-hover">
+			<button class="w3-button w3-text-white">FACILITIES&nbsp;&raquo;</button>
+			<div class="w3-dropdown-content w3-bar-block w3-card-4 w3-teal w3-topbar w3-border-black">
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Fish Farm</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Food Processing FActory</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Restaurant</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">E-learning</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Multi-purpose Hall</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Amphitheatre</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Sporting</a>
+
+			</div>
+		</div>
+		<div class="w3-dropdown-hover">
+			<button class="w3-button w3-text-white">CONTACT US&nbsp;&raquo;</button>
+			<div class="w3-dropdown-content w3-bar-block w3-card-4 w3-teal w3-topbar w3-border-black">
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Management Staff</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Heads of Department</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Academic Staff</a>
+				
+
+			</div>
+		</div>
+		<div class="w3-dropdown-hover">
+			<button class="w3-button w3-text-white">OUR FIRMS&nbsp;&raquo;</button>
+			<div class="w3-dropdown-content w3-bar-block w3-card-4 w3-teal w3-topbar w3-border-black">
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Fish Farm</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Food Processing</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Restaurant</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">E-learning</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Multi-purpose Hall</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Amphitheatre</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Sporting</a>
+
+			</div>
+		</div>
+		<div class="w3-dropdown-hover">
+			<button class="w3-button w3-text-white">EVENTS&nbsp;&raquo;</button>
+			<div class="w3-dropdown-content w3-bar-block w3-card-4 w3-teal w3-topbar w3-border-black">
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Inter-house Sports</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Cultural Day</a>
+				<a href="" class="w3-button w3-bar-item " style="text-decoration: none" style="text-decoration: none">Debate & Quiz Competition</a>
+				<a href="" class="w3-button w3-bar-item" style="text-decoration: none">Excursions</a>
+			</div>
+		</div>
+		<!-- <div class="w3-dropdown-hover">
+			<button class="w3-button w3-text-white">OFFERS></button>
+			<div class="w3-dropdown-content w3-bar-block w3-card-4 w3-teal w3-topbar w3-border-black">
+				<a href="" class="w3-button w3-bar-item">Scholarship</a>
+				
+			</div>
+		</div> -->
+		<div class="w3-dropdown-hover">
+			<button class="w3-button w3-text-white">GALLERY&nbsp;&raquo;</button>
+			<div class="w3-dropdown-content w3-bar-block w3-card-4 w3-teal w3-topbar w3-border-black">
+				<a href="" class="w3-button w3-bar-item">Life in FBS</a>
+			</div>
+		</div>
+		<div class="w3-dropdown-hover w3-left">
+				<button class="w3-button w3-text-white glyphicon glyphicon-log-in">&nbsp;LOG IN&nbsp;&raquo;</button>	
+		</div>
+		<div class="w3-dropdown-hover w3-left">
+				<button class="w3-button w3-text-white glyphicon glyphicon-shopping-cart">&nbsp;CART&nbsp;&raquo;
+				</button>
+				<div class="w3-dropdown-content w3-bar-block w3-card-4 w3-teal w3-topbar w3-border-black">
+				<a href="" class="w3-button w3-bar-item w3-right">View my Cart</a>
+			</div>	
+		</div>
+	</div>
+	</div>
+
+			<!-- main body begins -->
+
+
